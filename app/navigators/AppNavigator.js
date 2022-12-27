ae329b52180eaeceec1a262ad01a8649a796a68b
jeong-yewon <jdc209802@gmail.com>
2022-12-28 01:26:39 +0900

    Add: init

---
+++ app/navigators/AppNavigator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AppNavigator = () => {
+  <View>
+    <Text></Text>
+  </View>;
+};
+
+export default AppNavigator;
---
+++ app/screens/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Main = () => {
+  <View>
+    <Text>Main</Text>
+  </View>;
+};
+
+export default Main;
