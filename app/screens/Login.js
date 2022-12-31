6a3384a2c0e970a6b394cb0ab0e4b94526bc8335
jeong-yewon <jdc209802@gmail.com>
2023-01-01 03:07:54 +0900

    Feature: async storage 테스트 완료

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,19 +1,73 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
+// import styled from 'styled-components/native';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import {Alert, SafeAreaView, StyleSheet, Button, Platform} from 'react-native';
+// import {NaverLogin, getProfile} from '@react-native-seoul/naver-login';
+
+// const storeData = async value => {
+//   try {
+//     await AsyncStorage.setItem('@storage_Key', value);
+//   } catch (e) {
+//     // saving error
+//   }
+// };
+
+// storeData('바보');
+
+// const getData = async () => {
+//   try {
+//     const value = await AsyncStorage.getItem('@storage_Key');
+//     console.log(value);
+//     if (value !== null) {
+//       // value previously stored
+//     }
+//   } catch (e) {
+//     // error reading value
+//   }
+// };
+
+// getData();
+
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Button, View, Text, ScrollView} from 'react-native';
 import NaverLogin, {
   NaverLoginResponse,
   GetProfileResponse,
 } from '@react-native-seoul/naver-login';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const consumerKey = 'dSRuls9mwd8mLC5grvGa';
 const consumerSecret = 'nluxPu14So';
 const appName = '부둥부둥';
 const serviceUrlScheme = 'naverLogin';
 
+const storeData = async value => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('accessToken', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('accessToken');
+    console.log(JSON.parse(jsonValue).accessToken);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
+
 const Login = () => {
   const [success, setSuccessResponse] = useState();
   const [failure, setFailureResponse] = useState();
   const [getProfileRes, setGetProfileRes] = useState();
+
+  useEffect(() => {
+    storeData(success);
+  }, [success]);
 
   const login = async () => {
     const {failureResponse, successResponse} = await NaverLogin.login({
@@ -39,7 +93,9 @@ const Login = () => {
 
   const getProfile = async () => {
     try {
-      const profileResult = await NaverLogin.getProfile(accessToken);
+      const profileResult = await NaverLogin.getProfile(
+        'AAAAOD9PW46lMIltnGD+pxOah+uaf0VC1OySEfRyG3evp4b1pkANxjBcXEENikA4wnGa3UCx7ZCFAwBpH3wuCo5BCIc=',
+      );
       setGetProfileRes(profileResult);
     } catch (e) {
       setGetProfileRes(undefined);
@@ -86,10 +142,12 @@ const Login = () => {
         {getProfileRes ? (
           <ResponseJsonText name={'GetProfile'} json={getProfileRes} />
         ) : null}
+        <Button title={'storage'} onPress={getData} />
       </ScrollView>
     </SafeAreaView>
   );
 };
+
 const Gap = () => <View style={{marginTop: 24}} />;
 const ResponseJsonText = ({json = {}, name}) => (
   <View
